f494fc2cfb39ddc4747b3de1d6786544c7636f5c
Vlad Velici <vlad.velici@gmail.com>
2014-12-22 20:53:21 +0000

    Fixed: no new context on new request. Add: ViewParams context variable.

--- app/middleware.go
+++ app/middleware.go
@@ -13,15 +13,21 @@ type Ctrl func(*C) http.Handler
 // Request context
 type C struct {
 	CurrentUser *User
+	ViewParams  map[string]interface{}
 }
 
 // Create a context, create the controller using the context, and
 // create a http.Handler that combines all the middleware.
 func Mid(h Ctrl, mid ...Middleware) http.Handler {
-	ctx := new(C)
-	f := h(ctx)
-	for i := len(mid) - 1; i >= 0; i-- {
-		f = mid[i](ctx, f)
+	ret := func(w http.ResponseWriter, r *http.Request) {
+		ctx := new(C)
+		ctx.ViewParams = make(map[string]interface{})
+		ctx.ViewParams["PageTitle"] = "Quizz"
+		f := h(ctx)
+		for i := len(mid) - 1; i >= 0; i-- {
+			f = mid[i](ctx, f)
+		}
+		f.ServeHTTP(w, r)
 	}
-	return f
+	return http.HandlerFunc(ret)
 }
